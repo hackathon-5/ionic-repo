7cbc3830b7674fec8d1655dcf22a567a16bcdf54
Michael Feliciano <mfeliciano.enrique@gmail.com>
2015-08-29 13:56:14 -0400

    Last minute changes

--- www/js/controllers/taskController.js
+++ www/js/controllers/taskController.js
@@ -1,6 +1,8 @@
-bloomApp.controller('TaskController', function($ionicModal,$timeout,$rootScope,$scope,$ionicSideMenuDelegate,ionicMaterialInk,ionicMaterialMotion) {
+bloomApp.controller('TaskController', function($ionicLoading,$ionicPopup,$ionicModal,$timeout,$rootScope,$scope,$ionicSideMenuDelegate,ionicMaterialInk,ionicMaterialMotion) {
 
 	$scope.newTask = {};
+
+	$scope.isChecked = false;
 
 	$scope.data = [
 		{title : "Walk the Cat", value: 5},
@@ -8,6 +10,28 @@ bloomApp.controller('TaskController', function($ionicModal,$timeout,$rootScope,$
 		{title: "Practice Cello", value: 7},
 		{title: "Practice For Lacrosse",value: 7}
 	];
+
+	$scope.updatePoints = function(recipient){
+		//Update Points
+		console.log(recipient);
+		$ionicLoading.show({template: 'Awarded '+ recipient + ' points', duration:1000});
+		//$scope.isChecked = false;
+
+	};
+
+	$scope.awardPoints = function(){
+
+		$scope.pointData = {};
+
+		$ionicPopup.show({
+			template: '<button class="button button-calm button-full" ng-click="updatePoints(\'James\')">James Jones</button><button class="button button-calm button-full" ng-click="updatePoints(\'Sara\')" >Sara Jones</button> ',
+		  	title: 'Who Earned These Points?:',
+		  	scope: $scope,       
+		  	buttons: 
+		  	[{text: 'Close'}]
+		});
+
+	};
 
 	window.localStorage.taskData = JSON.stringify($scope.data);
 
